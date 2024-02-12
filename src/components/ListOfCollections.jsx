2409459d5499a1aa3bcbc967e61401769c516aaf
steven <Fernandezsteven185@gmail.com>
2024-02-12 00:40:14 -0600

    modal done, rendering collections

--- src/components/ListOfCollections.jsx
+++ src/components/ListOfCollections.jsx
@@ -4,18 +4,17 @@ export function ListOfCollections () {
   const [collections] = useState(
     JSON.parse(window.localStorage.getItem('listCollections')) || []
   )
-
+  // localStorage.clear()
   useEffect(() => {
-
+    console.log(collections)
   }, [collections])
-  console.log(collections)
 
   return (
     <ul>
       {collections.map((collection, index) => (
         <li key={index}>
           <p>{collection.collectionName}</p>
-          <p>{collection.id}</p>
+          <p>{collection.id}d</p>
         </li>
       ))}
     </ul>
