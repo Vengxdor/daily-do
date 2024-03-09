f133244e0d74b6a56dda5605372a645ce3f60dde
steven <Fernandezsteven185@gmail.com>
2024-03-08 19:02:55 -0600

    fix: put the user data from firebase in a context

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
+import { doc, getDoc } from 'firebase/firestore'
 import React, { createContext, useEffect, useState } from 'react'
+import { db } from '../firebase'
 
 export const UserAccountContext = createContext()
 
@@ -9,11 +11,38 @@ export function UserAccountProvider ({ children }) {
     return storageUser || null
   })
 
+  const [userData, setUserData] = useState(() => {
+    const storageData = JSON.parse(localStorage.getItem('userData'))
+    return storageData || null
+  })
+
+  useEffect(() => {
+    const getUserData = async () => {
+      if (!userAccount) return
+      // get the document from firebase
+      const documment = doc(db, 'Users', 'UserInfo')
+      const docSnapShot = await getDoc(documment)
+
+      if (!docSnapShot.exists()) return
+      // get the user data from the document
+      const data = docSnapShot.data()
+      const userData = data[userAccount.uid]
+
+      // store the data from the user
+      setUserData(userData)
+      localStorage.setItem('userData', JSON.stringify(userData))
+    }
+    getUserData()
+  }, [userAccount])
+
   useEffect(() => {
     window.localStorage.setItem('userAccount', JSON.stringify(userAccount))
   }, [userAccount])
+
   return (
-    <UserAccountContext.Provider value={{ userAccount, setUserAccount }}>
+    <UserAccountContext.Provider
+      value={{ userAccount, setUserAccount, userData, setUserData }}
+    >
       {children}
     </UserAccountContext.Provider>
   )
--- src/hooks/useUserAccount.jsx
+++ src/hooks/useUserAccount.jsx
@@ -1,24 +1,12 @@
 import { useContext } from 'react'
 import { UserAccountContext } from '../context/userContext'
-import { doc, getDoc } from 'firebase/firestore'
-import { db } from '../firebase'
 
 export function useUserAccount () {
-  const { userAccount, setUserAccount } = useContext(UserAccountContext)
+  const { userAccount, setUserAccount, userData, setUserData } = useContext(UserAccountContext)
 
-  const useDoc = async () => {
-    if (!userAccount) return
-    // get the document from firebase
-    const documment = doc(db, 'Users', 'UserInfo')
-    const docSnapShot = await getDoc(documment)
+  // const updateData = (listCollection, listTasks) => {
 
-    if (!docSnapShot.exists()) return
-    // get the user data from the document
-    const data = docSnapShot.data()
-    const userData = data[userAccount.uid]
-    console.log(userData)
-  }
-  useDoc()
+  // }
 
-  return { userAccount, setUserAccount }
+  return { userAccount, setUserAccount, userData, setUserData }
 }
