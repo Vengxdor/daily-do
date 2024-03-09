f63f7c8098424dbf6e0fada83a4a531fc3280cd9
steven <Fernandezsteven185@gmail.com>
2024-03-08 22:11:45 -0600

    fix: document name same as user id to easier access

--- src/service/dataCollection.js
+++ src/service/dataCollection.js
@@ -1,13 +1,18 @@
-import { addDoc, collection } from 'firebase/firestore'
+import { doc, setDoc } from 'firebase/firestore'
 import { db } from '../firebase'
 
 export const userCollections = async (user) => {
   if (!user) return
-  try {
-    // store the collection in firestore
-    const collectionArr = collection(db, 'Users')
 
-    await addDoc(collectionArr, {
+  try {
+    // Genera un identificador único para el documento
+    const documentId = user.uid // Reemplaza 'nombrePersonalizado' con tu lógica para generar un nombre único
+
+    // Obtiene una referencia al documento con el identificador único
+    const userDocRef = doc(db, 'Users', documentId)
+
+    // Al agregar el documento, proporciona un objeto con los datos del documento
+    await setDoc(userDocRef, {
       uid: user.uid,
       tasks: [],
       collections: []
