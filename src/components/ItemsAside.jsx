c4096f4a97feb0782f7e2ee1f5c53905cc57a518
Steven <122954695+Vengxdor@users.noreply.github.com>
2024-03-21 16:41:23 +0000

    fix: Aside links when the collection have a space in the name.

--- src/components/ItemsAside.jsx
+++ src/components/ItemsAside.jsx
@@ -5,14 +5,17 @@ import { Link, useLocation } from 'react-router-dom'
 function ItemsAside () {
   const { listCollections } = useCollection()
   const location = useLocation()
+  const currentLocation = location.pathname.replace('%20', '')
 
   return (
     <ul className='grid gap-3'>
       {listCollections.map((collection) => (
         <li
-          className={`rounded-tl-3xl rounded-bl-3xl ml-4 ${
-            location.pathname ===
-              `/${collection.collectionName}/${collection.id}` && 'active'
+          className={`rounded-tl-3xl rounded-bl-3xl ml-4 opacity-60 hover:opacity-100 transition-opacity duration-300 ${
+            currentLocation ===
+              `/${collection.collectionName.replace(' ', '')}/${
+                collection.id
+              }` && 'active'
           }`}
           key={collection.id}
         >
