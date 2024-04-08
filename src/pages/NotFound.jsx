a878cbbcaae202cdab0896e80f5fb96f24aace25
steven <Fernandezsteven185@gmail.com>
2024-04-07 19:06:18 -0500

    Fix: Change import path

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Logo from '../../public/Logo'
-
+import Logo from '../components/Logo'
 function NotFound () {
   return (
     <>
